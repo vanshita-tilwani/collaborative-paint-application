6892944df69ebdad4f490710c67fc689d13bb6aa
anizmo <potdar.anuj@gmail.com>
2023-03-31 09:37:38 -0400

    Adding Assignment 6 Part 2 to the final project     - Created dub project for server and client     - Added Assignment Part 2 to the project     - Started Integrating Assignment 6 with client and server code

---
+++ FinalProject/source/graphics_project/source/sdlapp.d
@@ -0,0 +1,101 @@
+import std.stdio;
+import std.string;
+
+import bindbc.sdl;
+import loader = bindbc.loader.sharedlib;
+import surface;
+
+SDL_Window* window;
+Surface imgSurface;
+const SDLSupport ret;
+
+shared static this() {
+
+    writeln("Starting the program");
+    version(Windows){
+        writeln("Searching for SDL on Windows");
+        ret = loadSDL("SDL2.dll");
+    }
+    version(OSX){
+        writeln("Searching for SDL on Mac");
+        ret = loadSDL();
+    }
+    version(linux){
+        writeln("Searching for SDL on Linux");
+        ret = loadSDL();
+    }
+
+    if (ret != sdlSupport) {
+        writeln("error loading SDL library");
+
+        foreach (info; loader.errors) {
+            writeln(info.error,':', info.message);
+        }
+    }
+
+    if (ret == SDLSupport.noLibrary) {
+        writeln("error no library found");
+    }
+
+    if (ret == SDLSupport.badLibrary) {
+        writeln("Eror badLibrary, missing symbols, perhaps an older or very new version of SDL is causing the problem?");
+    }
+
+    // Initialize SDL
+    if(SDL_Init(SDL_INIT_EVERYTHING) !=0){
+        writeln("SDL_Init: ", fromStringz(SDL_GetError()));
+    }
+
+    window = SDL_CreateWindow("D SDL Painting", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    imgSurface = Surface(640, 480);
+
+}
+
+shared static ~this() {
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    writeln("Ending application--good bye!");
+}
+
+class SDLApp {
+
+    bool runApplication = true;
+    bool drawing = false;
+
+    public:
+
+    void MainApplicationLoop() {
+        while (runApplication) {
+            SDL_Event e;
+
+            while (SDL_PollEvent(&e) != 0) {
+                if (e.type == SDL_QUIT) {
+                    runApplication = false;
+                }
+                else if (e.type == SDL_MOUSEBUTTONDOWN) {
+                    drawing = true;
+                }
+                else if (e.type == SDL_MOUSEBUTTONUP) {
+                        drawing = false;
+                    }
+                    else if (e.type == SDL_MOUSEMOTION && drawing) {
+                            int xPos = e.button.x;
+                            int yPos = e.button.y;
+
+                            int brushSize = 4;
+                            for (int w = -brushSize; w < brushSize; w++) {
+                                for (int h = -brushSize; h < brushSize; h++) {
+                                    imgSurface.updatePixel(xPos + w, yPos + h);
+                                }
+                            }
+                        }
+            }
+
+            SDL_BlitSurface(imgSurface.surface, null, SDL_GetWindowSurface(window), null);
+            SDL_UpdateWindowSurface(window);
+            SDL_Delay(16);
+        }
+    }
+}
+
+
