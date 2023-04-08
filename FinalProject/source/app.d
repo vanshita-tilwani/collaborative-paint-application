0bc47181e54351d09dd926246c94b82a503e29c3
Soufiane Jounaid <soufianej@Soufianes-MacBook-Pro.local>
2023-04-08 02:30:11 -0400

    Painting using Cairo

--- FinalProject/source/app.d
+++ FinalProject/source/app.d
@@ -1,75 +1,99 @@
 module pangocairo;
-
 import gio.Application : GioApplication = Application;
+
 import gtk.Application;
 import gtk.ApplicationWindow;
-
 import gtk.DrawingArea;
 import gtk.Widget;
 
-import cairo.Context;
-import cairo.ImageSurface;
+import gdk.Event;
+import gdk.Window;
+import gdk.c.functions;
 
-import pango.PgCairo;
-import pango.PgLayout;
-import pango.PgFontDescription;
+import cairo.c.types;
+import cairo.c.functions;
+
+import cairo.Context;
+import cairo.Surface;
+import cairo.ImageSurface;
 
 import std.stdio;
 import std.math;
 
-class PangoText : DrawingArea
+struct coords {
+	double x;
+	double y;
+}
+
+class DrawingCanvas : DrawingArea
 {
-	int m_radius = 150;
-	int m_nWords = 10;
-	string m_font = "Sans Bold 27";
+	coords[] draw_coords;	
+	bool drawing = false;
 
 	public this()
 	{
-		addOnDraw(&drawText);
+		addOnDraw(&drawPixel);
+		addOnMotionNotify(&onMouseMotion);
+		addOnButtonPress(&onMousePress);
+		addOnButtonRelease(&onButtonRelease);
+		
 	}
 
-	public bool drawText (Scoped!Context cr, Widget widget)
-	{
-		PgLayout layout;
-		PgFontDescription desc;
+	public bool onMouseMotion(Event event, Widget widget) {
+		bool value = false;
 
-		// Center coordinates on the middle of the region we are drawing
-		cr.translate(m_radius, m_radius);
-
-		// Create a PangoLayout, set the font and text
-		layout = PgCairo.createLayout(cr);
-
-		layout.setText("Text");
-		desc = PgFontDescription.fromString(m_font);
-		layout.setFontDescription(desc);
-		desc.free();
-
-		// Draw the layout m_nWords times in a circle
-		for (int i = 0; i < m_nWords; i++)
+		if(event.type == EventType.MOTION_NOTIFY && drawing == true)
 		{
-			int width, height;
-			double angle = (360. * i) / m_nWords;
-			double red;
-
-			cr.save();
-
-			/* Gradient from red at angle == 60 to blue at angle == 240 */
-			red   = (1 + cos ((angle - 60) * PI / 180.)) / 2;
-			cr.setSourceRgb(red, 0, 1.0 - red);
-
-			cr.rotate(angle * PI / 180.);
-
-			/* Inform Pango to re-layout the text with the new transformation */
-			PgCairo.updateLayout(cr, layout);
-
-			layout.getSize(width, height);
-			cr.moveTo( -(cast(double)width / PANGO_SCALE) / 2, - m_radius );
-			PgCairo.showLayout(cr, layout);
-
-			cr.restore();
+			GdkEventButton* mouseEvent = event.button;
+			writeln("Mouse motion ", mouseEvent.x, " ", mouseEvent.y);
+			draw_coords ~= [coords(mouseEvent.x, mouseEvent.y)];
+			widget.queueDraw();
+			value = true;
 		}
 
-		return true;
+		return(value);
+	}
+
+	public bool onMousePress(Event event, Widget widget) {
+		bool value = false;
+		
+		if(event.type == EventType.BUTTON_PRESS)
+		{
+			GdkEventButton* mouseEvent = event.button;
+			writeln("Mouse pressed ", mouseEvent.x, " ", mouseEvent.y);
+			draw_coords ~= [coords(mouseEvent.x, mouseEvent.y)];
+			widget.queueDraw();
+			value = true;
+			drawing = true;
+		}
+
+		return(value);
+	}
+
+	public bool onButtonRelease(Event event, Widget widget)
+	{
+		bool value = false;
+
+		if(event.type == EventType.BUTTON_RELEASE)
+		{
+			GdkEventButton* mouseEvent = event.button;
+			writeln("Mouse released ", mouseEvent.x, " ", mouseEvent.y);
+			value = true;
+			drawing = false;
+		}
+
+		return(value);
+	}
+
+	public bool drawPixel(Scoped!Context cr, Widget widget) {
+		foreach (coords coordinates ; draw_coords) {
+			cr.setLineWidth(5);
+			cr.setSourceRgba(0.1, 0.2, 0.3, 0.8);
+			cr.rectangle(coordinates.x, coordinates.y, 1, 1);
+			cr.stroke();
+		}
+
+		return(true);	
 	}
 }
 
@@ -78,18 +102,19 @@ int main(string[] args)
 {
 	Application application;
 
-	void activatePangoText(GioApplication app)
+	void activateCanvas(GioApplication app)
 	{
 		auto window = new ApplicationWindow(application);
-		window.setTitle("gtkD Pango text");
-		window.setDefaultSize(300, 300);
-		auto pt = new PangoText();
+		window.setTitle("Collaborative paint");
+		window.setDefaultSize(600, 600);
+		auto pt = new DrawingCanvas();
 		window.add(pt);
 		pt.show();
 		window.showAll();
+
 	}
 
-	application = new Application("org.gtkd.demo.pangocairo", GApplicationFlags.FLAGS_NONE);
-	application.addOnActivate(&activatePangoText);
+	application = new Application("org.dlangmafia.collabpaint", GApplicationFlags.FLAGS_NONE);
+	application.addOnActivate(&activateCanvas);
 	return application.run(args);
 }
